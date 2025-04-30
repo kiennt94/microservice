3668bd1d664c7e10f174011b1747f097ba1dcae6
unknown <karobest10@gmail.com>
2025-05-01 01:56:28 +0700

    clean service

--- vti-common/src/main/java/vti/common/exception_handler/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package vti.common.exception_handler;
-
-public class NotFoundException extends RuntimeException {
-  public NotFoundException(String message) {
-    super(message);
-  }
-}
--- vti-common/src/main/java/vti/common/service/CommonAccountService.java
+++
@@ -1,9 +0,0 @@
-package vti.common.service;
-
-import org.springframework.stereotype.Service;
-import vti.common.dto.AccountDto;
-
-@Service
-public interface CommonAccountService {
-    AccountDto findByUsername(String token);
-}
